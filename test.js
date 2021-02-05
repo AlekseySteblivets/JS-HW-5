99668814d4fd84441744fdfbdbc28e9baaaf1c14
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-05 14:03:22 +0200

    сохраняет решение задачи №15

--- test.js
+++ test.js
@@ -452,31 +452,56 @@
 // Стандартизируй публичный интерфейс класса заменив уже объявленные методы на геттеры и сеттеры brand,
 // model и price для взаимодействия с приватными свойствами.
 
+// class Car {
+//     #model;
+//     #price;
+//     #brand;
+
+//     constructor({ brand, model, price }) {
+//         this.#brand = brand;
+//         this.#model = model;
+//         this.#price = price;
+//     }
+
+//     get brand() {
+//         return this.#brand;
+//     }
+
+//     set brand(newBrand) {
+//         this.#brand = newBrand;
+//     }
+
+//     get model() {
+//         return this.#model;
+//     }
+
+//     set model(newModel) {
+//         this.#model = newModel;
+//     }
+
+//     get price() {
+//         return this.#price;
+//     }
+
+//     set price(newPrice) {
+//         this.#price = newPrice;
+//     }
+// }
+
+
+
+// 5-15-19Выполни рефакторинг класса Car. Добавь публичное статическое свойство MAX_PRICE со 
+// значением 50000 - максимально допустимая цена автомобиля.
+// Добавь сеттеру price проверку передаваемого значения параметра newPrice.Если оно больше чем MAX_PRICE,
+// сеттер ничего не делает, а если меньше или равно, то перезаписывает цену автомобиля.
+
 class Car {
-    #model;
+    // Пиши код ниже этой строки
+    static MAX_PRICE = 50000;
     #price;
-    #brand;
 
-    constructor({ brand, model, price }) {
-        this.#brand = brand;
-        this.#model = model;
+    constructor({ price }) {
         this.#price = price;
-    }
-
-    get brand() {
-        return this.#brand;
-    }
-
-    set brand(newBrand) {
-        this.#brand = newBrand;
-    }
-
-    get model() {
-        return this.#model;
-    }
-
-    set model(newModel) {
-        this.#model = newModel;
     }
 
     get price() {
@@ -484,6 +509,22 @@ class Car {
     }
 
     set price(newPrice) {
+        if (newPrice > Car.MAX_PRICE) {
+            return;
+        }
         this.#price = newPrice;
     }
+
+
+
+    // Пиши код выше этой строки
 }
+
+const audi = new Car({ price: 35000 });
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000
