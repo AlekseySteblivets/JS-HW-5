ab38f0475893a1b950d8e9e9ea66b4e3a730867f
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-05 16:24:34 +0200

    сохраняет решение задачи №18

--- test.js
+++ test.js
@@ -565,6 +565,41 @@
 // Добавь классу Admin публичное статическое свойство AccessLevel(уровень доступа),
 // значение которого это объект { BASIC: 'basic', SUPERUSER: 'superuser' }.
 
+// class User {
+//     email;
+
+//     constructor(email) {
+//         this.email = email;
+//     }
+
+//     get email() {
+//         return this.email;
+//     }
+
+//     set email(newEmail) {
+//         this.email = newEmail;
+//     }
+// }
+// // Пиши код ниже этой строки
+
+// class Admin extends User {
+//     static AccessLevel = { BASIC: 'basic', SUPERUSER: 'superuser' };
+//     constructor() {
+//         super();
+//     }
+
+// }
+
+// console.log(Admin.AccessLevel);
+// console.log(Admin);
+
+
+
+// 5-18-19Добавь классу Admin метод constructor, который принимает один параметр - 
+// объект настроек с двумя свойствами email и accessLevel.
+// Добавь классу Admin публичное свойсво accessLevel, значение которого будет передаваться при вызове конструктора.
+// Чтобы показать как будет использоваться класс Admin, мы добавили инициализацию экземпляра под объявлением класса.
+
 class User {
     email;
 
@@ -580,15 +615,28 @@ class User {
         this.email = newEmail;
     }
 }
-// Пиши код ниже этой строки
 
 class Admin extends User {
-    static AccessLevel = { BASIC: 'basic', SUPERUSER: 'superuser' };
-    constructor() {
-        super();
+    // Пиши код ниже этой строки
+    accessLevel;
+
+    static AccessLevel = {
+        BASIC: 'basic',
+        SUPERUSER: 'superuser'
+    };
+    constructor({ email, accessLevel }) {
+        super(email);
+        this.accessLevel = accessLevel;
+
     }
 
+    // Пиши код выше этой строки
 }
 
-console.log(Admin.AccessLevel);
-console.log(Admin);
+const mango = new Admin({
+    email: 'mango@mail.com',
+    accessLevel: Admin.AccessLevel.SUPERUSER
+});
+
+console.log(mango.email); // mango@mail.com
+console.log(mango.accessLevel); // superuser
