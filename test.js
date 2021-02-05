b9a38447411b8be1e9ecd6f026595951a4486186
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-05 14:32:22 +0200

    сохраняет решение хадачи №16

--- test.js
+++ test.js
@@ -495,36 +495,64 @@
 // Добавь сеттеру price проверку передаваемого значения параметра newPrice.Если оно больше чем MAX_PRICE,
 // сеттер ничего не делает, а если меньше или равно, то перезаписывает цену автомобиля.
 
+// class Car {
+//     // Пиши код ниже этой строки
+//     static MAX_PRICE = 50000;
+//     #price;
+
+//     constructor({ price }) {
+//         this.#price = price;
+//     }
+
+//     get price() {
+//         return this.#price;
+//     }
+
+//     set price(newPrice) {
+//         if (newPrice > Car.MAX_PRICE) {
+//             return;
+//         }
+//         this.#price = newPrice;
+//     }
+//     // Пиши код выше этой строки
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+
+
+// 5-16-19Добавь классу Car публичный статический метод checkPrice(price), принимающий цену автомобиля. 
+// Метод должен сравнить значения параметра price и приватного статического свойства MAX_PRICE.
+// Если цена автомобиля превышает максимальную, метод должен вернуть строку 'Внимание! Цена превышает допустимую.'.
+// В противном случае метод должен вернуть строку 'Всё хорошо, цена в порядке.'.
+// Под объявлением класса мы добавили инициализацию экземпляра и вызовы методов,
+// чтобы показать как будет использоваться метод checkPrice(price).
+
 class Car {
+    static #MAX_PRICE = 50000;
     // Пиши код ниже этой строки
-    static MAX_PRICE = 50000;
-    #price;
-
-    constructor({ price }) {
-        this.#price = price;
-    }
-
-    get price() {
-        return this.#price;
-    }
-
-    set price(newPrice) {
-        if (newPrice > Car.MAX_PRICE) {
-            return;
+    static checkPrice(price) {
+        if (price > Car.#MAX_PRICE) {
+            return 'Внимание! Цена превышает допустимую.';
         }
-        this.#price = newPrice;
+        return 'Всё хорошо, цена в порядке.';
     }
-
-
 
     // Пиши код выше этой строки
+    constructor({ price }) {
+        this.price = price;
+    }
 }
 
-const audi = new Car({ price: 35000 });
-console.log(audi.price); // 35000
+const audi = new Car({ price: 36000 });
+const bmw = new Car({ price: 64000 });
 
-audi.price = 49000;
-console.log(audi.price); // 49000
-
-audi.price = 51000;
-console.log(audi.price); // 49000
+console.log(Car.checkPrice(audi.price)); // Всё хорошо, цена в порядке.
+console.log(Car.checkPrice(bmw.price)); // Внимание! Цена превышает допустимую.
