e367f48ad168b9a147d2222d408fc86539a5532b
Aleksey Steblivets <aleksey.steblivets@gmail.com>
2021-02-05 11:18:00 +0200

    сохраняет решение задачи №15

--- test.js
+++ test.js
@@ -353,34 +353,95 @@
 // в которой твой код будут проверять тесты.
 // Пожалуйста ничего там не меняй.
 
-class Storage {
-    #items;
-    constructor(items) {
-        this.#items = items;
+// class Storage {
+//     #items;
+//     constructor(items) {
+//         this.#items = items;
+//     }
+
+//     getItems() {
+//         return this.#items;
+//     };
+
+//     addItem(newItem) {
+//         this.#items.push(newItem);
+//     };
+
+//     removeItem(item) {
+//         const itemIndex = this.#items.indexOf(item);
+//         this.#items.splice(itemIndex, 1);
+//     };
+
+// }
+
+// // ??не пойму как меняется в ватче items???
+
+
+// // Пиши код выше этой строки
+// const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem("Дроид");
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem("Пролонгер");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+
+
+
+// 5-13-19Выполни рефакторинг заменив функцию-конструктор StringBuilder на класс с методами.
+// Сделай так, чтобы свойство value было приватным.
+// Под комментарием мы добавили инициализацию экземпляра и вызовы методов в той последовательности,
+// в которой твой код будут проверять тесты.
+// Пожалуйста ничего там не меняй.
+
+class StringBuilder {
+    #value;
+    constructor(baseValue) {
+        this.#value = baseValue;
     }
-
-    getItems() {
-        return this.#items;
-    };
-
-    addItem(newItem) {
-        this.#items.push(newItem);
-    };
-
-    removeItem(item) {
-        const itemIndex = this.#items.indexOf(item);
-        this.#items.splice(itemIndex, 1);
-    };
-
+    getValue() {
+        return this.#value;
+    }
+    padEnd(str) {
+        this.#value += str;
+    }
+    padStart(str) {
+        this.#value = str + this.#value;
+    }
+    padBoth(str) {
+        this.padStart(str);
+        this.padEnd(str);
+    }
 }
 
-// ??не пойму как меняется в ватче items???
 
+
+// function StringBuilder(baseValue) {
+//     this.value = baseValue;
+// }
+
+// StringBuilder.prototype.getValue = function () {
+//     return this.value;
+// };
+
+// StringBuilder.prototype.padEnd = function (str) {
+//     this.value += str;
+// };
+
+// StringBuilder.prototype.padStart = function (str) {
+//     this.value = str + this.value;
+// };
+
+// StringBuilder.prototype.padBoth = function (str) {
+//     this.padStart(str);
+//     this.padEnd(str);
+// };
 
 // Пиши код выше этой строки
-const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
-console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
-storage.addItem("Дроид");
-console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
-storage.removeItem("Пролонгер");
-console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // '.'
+builder.padStart('^');
+console.log(builder.getValue()); // '^.'
+builder.padEnd('^');
+console.log(builder.getValue()); // '^.^'
+builder.padBoth('=');
+console.log(builder.getValue()); // '=^.^='
